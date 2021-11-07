3d91694b991351b57b2105f16c21cf77c3232ecd
RdotSilva <RyandotSilva@gmail.com>
2021-11-07 07:19:04 -0500

    Add Home component template

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import * as React from "react";
 import { ChakraProvider, Box, theme } from "@chakra-ui/react";
 import NavBar from "./components/ui/NavBar";
 import { Logo } from "./Logo";
+import Home from "./components/Home";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <NavBar />
+    <Home />
   </ChakraProvider>
 );
