a0f70ee9cbc9c7158d125caa7742ea49a4c859d8
RdotSilva <RyandotSilva@gmail.com>
2021-11-08 14:01:23 -0500

    Add Profile component

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="">
+                <i className="fa fa-twitter" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
