31935be3a40223b4381447e0b30ac0439b496884
RdotSilva <RyandotSilva@gmail.com>
2021-11-09 13:39:15 -0500

    Add social media links

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -7,8 +7,14 @@ const Profile = () => {
         <div className="profile-details">
           <div className="colz">
             <div className="colz-icon">
-              <a href="">
+              <a href="https://twitter.com/rdotsilva">
                 <i className="fa fa-twitter" />
+              </a>
+              <a href="https://github/rdotsilva">
+                <i className="fa fa-github" />
+              </a>
+              <a href="https://www.linkedin.com/in/rdotsilva/">
+                <i className="fa fa-linkedin" />
               </a>
             </div>
           </div>
