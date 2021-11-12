73423b8cae1737399235b38a91b0a1d5cfef1f0a
RdotSilva <RyandotSilva@gmail.com>
2021-11-12 18:57:00 +0000

    Add logic to get current sceen

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { SCREENS, getScreenIndex } from "../../utils/screenUtils";
+import ScrollService from "../../services/ScrollService";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
+  const [selectedScreen, setSelectedScreen] = useState(0);
+  const [showHeaderOptions, setShowHeaderOptions] = useState(false);
+
+  const updateCurrentScreen = (currentScreen: any) => {
+    if (!currentScreen || !currentScreen.screenInView) return;
+
+    let screenIndex = getScreenIndex(currentScreen.screenInView);
+    if (screenIndex < 0) return;
+  };
+
+  let currentScreenSubscription =
+    ScrollService.currentScreenBroadcaster.subscribe(updateCurrentScreen);
+
   return <div>Header</div>;
 };
 
