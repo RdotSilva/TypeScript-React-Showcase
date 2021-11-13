36e82bafdb67c2014a98c2419dd42ab38f6511ff
RdotSilva <RyandotSilva@gmail.com>
2021-11-13 09:15:17 +0000

    Add Header display

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -48,7 +48,33 @@ const Header = () => {
     setShowHeaderOptions(false);
   };
 
-  return <div>Header</div>;
+  return (
+    <div
+      className="header-container"
+      onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+    >
+      <div className="header-parent">
+        <div
+          className="header-hamburger"
+          onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+        >
+          <FontAwesomeIcon className="header-hamburger-bars" icon={faBars} />
+        </div>
+        <div className="header-logo">
+          <span>RYAN</span>
+        </div>
+        <div
+          className={
+            showHeaderOptions
+              ? "header-options show-hamburger-options"
+              : "header-options"
+          }
+        >
+          {getHeaderOptions()}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
