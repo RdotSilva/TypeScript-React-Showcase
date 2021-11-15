2767e76f9e57119205e208f22be028aaf376b04d
RdotSilva <RyandotSilva@gmail.com>
2021-11-15 19:14:19 +0000

    Add AboutMe component

---
+++ src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SectionHeading from "../SectionHeading/SectionHeading";
+import ScrollService from "../../services/ScrollService";
+import Animations from "../../services/Animations";
+
+const AboutMe = () => {
+  return (
+    <div>
+      <SectionHeading />
+    </div>
+  );
+};
+
+export default AboutMe;
