6de3cde851eb54bf9ff79c3da123fd0716ba596b
RdotSilva <RyandotSilva@gmail.com>
2021-11-15 19:34:33 +0000

    Add skeleton for rendering details

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -3,10 +3,35 @@ import SectionHeading from "../SectionHeading/SectionHeading";
 import ScrollService from "../../services/ScrollService";
 import Animations from "../../services/Animations";
 
-const AboutMe = () => {
+const AboutMe = ({ id }) => {
   return (
-    <div>
-      <SectionHeading />
+    <div className="about-me-container screen-container fade-in" id={id || ""}>
+      <div className="about-me-parent">
+        <SectionHeading title={"About Me"} subHeading={"Why Choose Me?"} />
+        <div className="about-me-card">
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <span className="about-me-description">
+              Full Stack Software Engineer
+            </span>
+            <div className="about-me-highlights">
+              <div className="highlight-heading">
+                <span>My skills:</span>
+              </div>
+              // TODO: Render highlights
+            </div>
+            <div className="about-me-options">
+              <button
+                className="btn primary-btn"
+                // onClick={} TODO: Add onCLick handler to scroll to Contact me section
+              >
+                {" "}
+                Contact Me{" "}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
