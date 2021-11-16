7db2d13073f4412f4ef9a47e74858734d1d4c041
RdotSilva <RyandotSilva@gmail.com>
2021-11-16 19:08:31 +0000

    Add profileData to render in AboutMe

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -11,6 +11,25 @@ const AboutMe = ({ id }) => {
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
+  // Profile data that I will render in the AboutMe section
+  const profileData = {
+    description:
+      "Full Stack Software Engineer who enjoys being challenged and working on projects that bring me outside of my comfort zone. Always furthering self-improvement by learning new languages, technologies, and development techniques.",
+    highlights: {
+      bullets: ["Continuous learner", "Persistence", "Exploring new ideas"],
+      heading: "Here are a Few Highlights:",
+    },
+  };
+
+  const renderHighlight = () => {
+    return profileData.highlights.bullets.map((value, i) => (
+      <div className="highlight" key={i}>
+        <div className="highlight-blob"></div>
+        <span>{value}</span>
+      </div>
+    ));
+  };
+
   return (
     <div className="about-me-container screen-container fade-in" id={id || ""}>
       <div className="about-me-parent">
@@ -23,18 +42,21 @@ const AboutMe = ({ id }) => {
             </span>
             <div className="about-me-highlights">
               <div className="highlight-heading">
-                <span>My skills:</span>
+                <span>{profileData.highlights.heading}</span>
               </div>
-              // TODO: Render highlights
+              {renderHighlight()}
             </div>
             <div className="about-me-options">
               <button
                 className="btn primary-btn"
-                // onClick={} TODO: Add onCLick handler to scroll to Contact me section
+                onClick={() => ScrollService.scrollHandler.scrollToContactMe()}
               >
                 {" "}
                 Contact Me{" "}
               </button>
+              <a href="#">
+                <button className="btn highlighted-btn">See My Work</button>
+              </a>
             </div>
           </div>
         </div>
