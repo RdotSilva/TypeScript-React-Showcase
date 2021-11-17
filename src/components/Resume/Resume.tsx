93830641bee7c33e46369b4f957d6bd813ccffb0
RdotSilva <RyandotSilva@gmail.com>
2021-11-17 20:06:17 +0000

    Set up Resume component

---
+++ src/components/Resume/Resume.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import Animations from "../../services/Animations";
+import ScrollService from "../../services/ScrollService";
+import SectionHeading from "../SectionHeading/SectionHeading";
+
+const Resume = (props) => {
+
+  return (
+    <div>Resume</div>
+};
+
+export default Resume;
