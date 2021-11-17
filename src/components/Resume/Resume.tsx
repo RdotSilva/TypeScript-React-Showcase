11ec012b931e51458660f0b4a30947764dadb007
RdotSilva <RyandotSilva@gmail.com>
2021-11-17 20:07:15 +0000

    Update Resume display

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -4,9 +4,36 @@ import ScrollService from "../../services/ScrollService";
 import SectionHeading from "../SectionHeading/SectionHeading";
 
 const Resume = (props) => {
+  const [selectedBulletIndex, setSelectedBulletIndex] = useState(0);
+  const [carousalOffsetStyle, setCarousalOffsetStyle] = useState({});
+
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+
+    Animations.animations.fadeInScreen(props.id);
+  };
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
   return (
-    <div>Resume</div>
+    <div
+      className="resume-container screen-container fade-in"
+      id={props.id || ""}
+    >
+      <div className="resume-content">
+        <SectionHeading title={"Resume"} subHeading={"My formal Bio Details"} />
+        <div className="resume-card">
+          <div className="resume-bullets">
+            <div className="bullet-container">
+              <div className="bullet-icons"></div>
+              <div className="bullets">{getBullets()}</div>
+            </div>
+          </div>
+          <div className="resume-bullet-details">{getResumeScreens()}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
