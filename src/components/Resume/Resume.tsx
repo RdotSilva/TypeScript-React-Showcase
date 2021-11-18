3aba777b790ca31e795ce76c1f4af34117f086eb
RdotSilva <RyandotSilva@gmail.com>
2021-11-18 19:15:08 +0000

    Add static data for bullets and skills

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -32,6 +32,22 @@ const Resume = (props) => {
     );
   };
 
+  const resumeBullets = [
+    { label: "Education", logoSrc: "education.svg" },
+    { label: "Work History", logoSrc: "work-history.svg" },
+    { label: "Programming Skills", logoSrc: "programming-skills.svg" },
+    { label: "Projects", logoSrc: "projects.svg" },
+    { label: "Interests", logoSrc: "interests.svg" },
+  ];
+
+  const skillDetails = [
+    { skill: "JavaScript", ratingPercentage: 82 },
+    { skill: "React JS", ratingPercentage: 82 },
+    { skill: "Node JS", ratingPercentage: 80 },
+    { skill: "HTML", ratingPercentage: 80 },
+    { skill: "CSS", ratingPercentage: 80 },
+  ];
+
   let fadeInScreenHandler = (screen) => {
     if (screen.fadeInScreen !== props.id) return;
 
