a1a23b32559557974e0eaa0803d91416cea2ced7
RdotSilva <RyandotSilva@gmail.com>
2021-11-22 19:38:29 +0000

    Add Projects component

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SectionHeading from "../SectionHeading/SectionHeading";
+import ScrollService from "../../services/ScrollService";
+import Animations from "../../services/Animations";
+import "./Projects.css";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+
+const Projects = () => {
+  return <div>Projects</div>;
+};
+
+export default Projects;
