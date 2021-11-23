d39e2d06898b407019076e0ac4fa796d5776d274
RdotSilva <RyandotSilva@gmail.com>
2021-11-23 20:10:21 +0000

    Set up container for projects section

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -6,8 +6,26 @@ import "./Projects.css";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
-const Projects = () => {
-  return <div>Projects</div>;
+const Projects = (props) => {
+  return (
+    <div>
+      <SectionHeading
+        title={"My projects"}
+        subHeading={"Check out what I've been working on"}
+      />
+      <section className="projects-section fade-in" id={props.id || ""}>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="testi-item">
+                <div className="testi-comment"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Projects;
