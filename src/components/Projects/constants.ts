bdef00a5f94540e9457bab66f4854022d8efe32e
RdotSilva <RyandotSilva@gmail.com>
2021-11-25 20:20:37 +0000

    Add projects constants

---
+++ src/components/Projects/constants.ts
@@ -0,0 +1,7 @@
+export const projects = [
+  {
+    title: "Developer Social Network",
+    description: "This was the first React project I worked on",
+    skills: "React, JavaScript, MongoDB, ExpressJS, NodeJS",
+  },
+];
