58069e0169f4abb4a27b2d2194735f964ece2daf
RdotSilva <RyandotSilva@gmail.com>
2021-11-27 11:44:00 +0000

    Move card display into new component

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const ProjectCard = () => {
-  return <div>ProjectCard</div>;
+  return (
+    <div className="col-lg-12">
+      <div className="project-item">
+        <div className="project-comment">
+          <p>
+            <i className="fa fa-quote-left" />
+            {project.description}
+            <i className="fa fa-quote-right" />
+          </p>
+        </div>
+        <div className="project-info">
+          {/* TODO: Add project image <img src={} alt="no internet connection"></img> */}
+          <h5>{project.title}</h5>
+          <p>{project.skills}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectCard;
