89c620b35981520cf554a9c5e91c3ae49a3a4fe1
RdotSilva <RyandotSilva@gmail.com>
2021-11-27 11:48:58 +0000

    Add ProjectCardProps type

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,20 +1,26 @@
 import React from "react";
 
-const ProjectCard = () => {
+type ProjectCardProps = {
+  description: string;
+  title: string;
+  skills: string;
+};
+
+const ProjectCard = ({ description, title, skills }: ProjectCardProps) => {
   return (
     <div className="col-lg-12">
       <div className="project-item">
         <div className="project-comment">
           <p>
             <i className="fa fa-quote-left" />
-            {project.description}
+            {description}
             <i className="fa fa-quote-right" />
           </p>
         </div>
         <div className="project-info">
           {/* TODO: Add project image <img src={} alt="no internet connection"></img> */}
-          <h5>{project.title}</h5>
-          <p>{project.skills}</p>
+          <h5>{title}</h5>
+          <p>{skills}</p>
         </div>
       </div>
     </div>
