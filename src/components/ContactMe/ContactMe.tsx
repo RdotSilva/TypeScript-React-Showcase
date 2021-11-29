b450cdc50a6c77c009dd06fdba4cdceca18ac118
RdotSilva <RyandotSilva@gmail.com>
2021-11-29 19:12:10 +0000

    Set up container structure

--- src/components/ContactMe/ContactMe.tsx
+++ src/components/ContactMe/ContactMe.tsx
@@ -1,7 +1,23 @@
+import { ReactTypical } from "@deadcoder0904/react-typical";
 import React from "react";
+import SectionHeading from "../SectionHeading/SectionHeading";
 
-const ContactMe = () => {
-  return <div>ContactMe</div>;
+const ContactMe = (props) => {
+  return (
+    <div className="main-container fade-in" id={props.id || ""}>
+      <SectionHeading subHeading={"Want to connect?"} title={"Contact Me"} />
+      <div className="central-form">
+        <div className="col"></div>
+        <div className="back-form">
+          <div className="img-back">
+            <h4>Send Your Email Here!</h4>
+            // TODO: Add image
+            {/* <img src={} alt="image not found" /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactMe;
