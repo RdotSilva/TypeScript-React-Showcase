1b7804b9dd8c3634d474e18c0dd9f0c58781bf8b
RdotSilva <RyandotSilva@gmail.com>
2021-12-03 20:21:34 +0000

    Update display for project

--- src/components/Projects/Project/Project.tsx
+++ src/components/Projects/Project/Project.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Box, Image, Badge, StarIcon } from "@chakra-ui/react";
 
-const Project = () => {
-  return <div>Project</div>;
+const Project = ({ description, title, skills }) => {
+  return (
+    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+      <Image src={project.imageUrl} alt={project.imageAlt} />
+
+      <Box p="6">
+        <Box display="flex" alignItems="baseline">
+          <Badge borderRadius="full" px="2" colorScheme="teal">
+            React
+          </Badge>
+          <Box
+            color="gray.500"
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="xs"
+            textTransform="uppercase"
+            ml="2"
+          >
+            Full stack &bull; Web app
+          </Box>
+        </Box>
+
+        <Box
+          mt="1"
+          fontWeight="semibold"
+          as="h4"
+          lineHeight="tight"
+          isTruncated
+        >
+          {project.title}
+        </Box>
+
+        <Box>
+          {project.description}
+          <Box as="span" color="gray.600" fontSize="sm"></Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Project;
